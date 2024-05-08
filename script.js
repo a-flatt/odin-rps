e353da6c67e13140891e0dc7cdf966904b5c8ff6
Adam Platt <adamjplatt@outlook.com>
2024-05-09 07:37:24 +0800

    add round component to playGame

--- script.js
+++ script.js
@@ -30,16 +30,22 @@ function playRound (humanChoice, computerChoice) {
         console.log("That's a tie.");
     } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
         console.log("You win.");
+        humanScore += 1;
     } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
         console.log('You win.');
+        humanScore += 1;
     } else if (humanChoice == 'paper' && computerChoice == 'rock') {
         console.log('You win.');
+        humanScore += 1;
     } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         console.log('You lose.');
+        computerScore += 1;
     } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         console.log('You lose.');
+        computerScore += 1;
     } else if (humanChoice == 'rock' && computerChoice == 'paper') {
         console.log('You lose.');
+        computerScore += 1;
     } else {
         console.log("I think you inputted an invalid choice.");
     }
@@ -47,8 +53,11 @@ function playRound (humanChoice, computerChoice) {
 
 function playGame () {
 
-    const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
-
-    playRound(humanChoice, computerChoice);
+    for (let i = 0; i < 5; i++) {
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice);    
+    }
+    console.log("Human Score: ", humanScore);
+    console.log("Computer Score: ", computerScore);
 }
