10b55bb7e0236ae4875acb309b154977e3338c0a
Adam Platt <adamjplatt@outlook.com>
2024-05-09 07:32:26 +0800

    finish off playRound logic

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
 
     randomInt = Math.floor(Math.random() * 3);
     if (randomInt == 0) {
-        console.log("rock");
+        return "rock";
     } else if (randomInt == 1) {
-        console.log("paper");
+        return "paper";
     } else {
-        console.log("scissors");
+        return "scissors";
     }
 }
 
 function getHumanChoice () {
 
     choice = prompt("Please enter your choice");
-    console.log(choice);
+    choice.toLowerCase();
+    return choice;
+}
+
+function playRound (humanChoice, computerChoice) {
+
+    // paper beats rock
+    // rock beats scissors
+    // scissors beats paper
+
+    if (humanChoice == computerChoice) {
+        console.log("That's a tie.");
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log("You win.");
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        console.log('You win.');
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log('You win.');
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        console.log('You lose.');
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        console.log('You lose.');
+    } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        console.log('You lose.');
+    } else {
+        console.log("I think you inputted an invalid choice.");
+    }
+}
+
+function playGame () {
+
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice();
+
+    playRound(humanChoice, computerChoice);
 }
